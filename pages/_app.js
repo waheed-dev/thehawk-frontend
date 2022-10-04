8ab9c05a771071ac101adae1510f5d8add0bcead
Akash Mahmud <mahmudakash177@gmail.com>
2022-10-04 17:38:47 +0600

    add progress bar on component hydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import "@/css/bootstrap.min.css";
 import "@/css/style.css";
 import '../styles/globals.css'
-
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
+
   return (
-
-
-      
-        <Component {...pageProps} />
-
-
+    <>
+      <NextNProgress color="#e00000"  />
+      <Component {...pageProps} />
+    </>
   );
 }
 
